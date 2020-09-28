634ce19d58340b90e3464b0b99c5871b5dce6c26
Alex Curnow <alexcurnow@gmail.com>
2020-09-28 10:41:38 -0500

    initial commit

---
+++ post/Post.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace post
+{
+    public class Post
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        private int _upVoteCount { get; set; }
+        private int _downVoteCount { get; set; }
+
+        public Post(string author, string title, string content)
+        {
+            Author = author;
+            Title = title;
+            Content = content;
+            
+        }
+        
+        public int upVote(int upVotes)
+        {
+            return _upVoteCount += upVotes;
+        }
+
+        public int downVote(int downVotes)
+        {
+            return _downVoteCount += downVotes;
+        }
+
+        public void display()
+        {
+            Console.WriteLine($"{this.Title} by {this.Author}: {this.Content}");
+            Console.WriteLine($"UpVotes: {this._upVoteCount} // DownVotes: {this._downVoteCount}");
+        }
+    }
+}
---
+++ post/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace post
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newPost = new Post("Alex Curnow", "First Post", "This is my first post!");
+
+            newPost.upVote(5);
+            newPost.upVote(50);
+            newPost.downVote(12);
+
+            newPost.display();
+        }
+    }
+}
